b77a5d042834bbbc19e167a13c900387f83dff87
Olaf Sulich <olafsulich@gmail.com>
2021-01-29 13:35:58 +0100

    Create and test function getGreatestPlayer

--- finish/src/match.ts
+++ finish/src/match.ts
@@ -6,3 +6,10 @@ export function getAllPlayers<T extends Player[], U extends Player[]>(
 ) {
   return [...team1, ...team2];
 }
+
+export function getGreatestPlayer(players: Player[]) {
+  const playerWithHighestScore = players.reduce((prev, curr) => {
+    return prev.points > curr.points ? prev : curr;
+  });
+  return playerWithHighestScore;
+}
--- finish/tests/match.spec.ts
+++ finish/tests/match.spec.ts
@@ -1,7 +1,13 @@
-import { getAllPlayers } from "../src/match";
+import { getAllPlayers, getGreatestPlayer } from "../src/match";
 import { team1, team2, allPlayers } from "../data/teams";
 
 it("returns all players in match", () => {
   const result = getAllPlayers(team1, team2);
   expect(result).toEqual(allPlayers);
+});
+
+it("returns player with most points", () => {
+  const players = getAllPlayers(team1, team2);
+  const greatestPlayer = getGreatestPlayer(players);
+  expect(greatestPlayer.name).toBe("Blake Griffin");
 });
